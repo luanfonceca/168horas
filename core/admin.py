43c716466be2d50f1f9d9fb29aae7a8aa6739e45
Luan fonseca de farias <luanfonceca@gmail.com>
2015-12-17 15:31:06 -0300

    Add profile to api

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
