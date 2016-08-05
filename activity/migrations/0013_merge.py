be15a9efedf725944c2e49f59c4d793a4fbea12f
Luan fonseca de farias <luanfonceca@gmail.com>
2016-08-05 08:17:14 -0300

    Add merge migration

---
+++ activity/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('activity', '0012_activity_organizers'),
+        ('activity', '0011_activity_embedded_schedule'),
+    ]
+
+    operations = [
+    ]
