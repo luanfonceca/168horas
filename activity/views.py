d93e01aa1b2995fb25f4e73953afa85c3fc97fea
Luan fonseca de farias <luanfonceca@gmail.com>
2016-09-28 17:59:08 -0300

    Fix Non-ASCII on the activity views

--- activity/views.py
+++ activity/views.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
 from django.http import HttpResponseRedirect
